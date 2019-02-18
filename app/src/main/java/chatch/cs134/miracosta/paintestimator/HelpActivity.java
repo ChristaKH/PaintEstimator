16d3c931074f987154c7c867e2d3fe6bc76cbfd0
Christa Hatch <christahatch1994@pointloma.edu>
2019-02-17 22:20:18 -0800

    Declare edit texts Declare buttons Instantiate edit texts Instantiate buttons

--- app/src/main/java/chatch/cs134/miracosta/paintestimator/HelpActivity.java
+++ app/src/main/java/chatch/cs134/miracosta/paintestimator/HelpActivity.java
@@ -2,11 +2,16 @@ package chatch.cs134.miracosta.paintestimator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class HelpActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity
+{
+
+    Button inputDimenionsButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
     }
--- app/src/main/java/chatch/cs134/miracosta/paintestimator/MainActivity.java
+++ app/src/main/java/chatch/cs134/miracosta/paintestimator/MainActivity.java
@@ -2,12 +2,34 @@ package chatch.cs134.miracosta.paintestimator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText lengthEditText;
+    EditText widthEditText;
+    EditText heightEditText;
+    EditText doorsEditText;
+    EditText windowsEditText;
+
+    Button computeButton;
+    Button helpButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lengthEditText = findViewById(R.id.lengthEditText);
+        widthEditText = findViewById(R.id.widthEditText);
+        heightEditText = findViewById(R.id.heightEditText);
+        doorsEditText = findViewById(R.id.doorsEditText);
+        windowsEditText = findViewById(R.id.windowsEditText);
+
+        computeButton = findViewById(R.id.computeButton);
+        helpButton = findViewById(R.id.helpButton);
     }
 }
