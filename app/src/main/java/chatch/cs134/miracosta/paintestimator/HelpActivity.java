d179090ff5cb0133061077e0c017216d7e6611da
Christa Hatch <christahatch1994@pointloma.edu>
2019-02-18 16:55:28 -0800

    Add intents to switch activities

--- app/src/main/java/chatch/cs134/miracosta/paintestimator/HelpActivity.java
+++ app/src/main/java/chatch/cs134/miracosta/paintestimator/HelpActivity.java
@@ -1,18 +1,37 @@
 package chatch.cs134.miracosta.paintestimator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity
 {
-
-    Button inputDimenionsButton;
+    TextView estimatedPaintTextView;
+    Button inputDimensionsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        inputDimensionsButton = findViewById(R.id.inputDimensionsButton);
+        estimatedPaintTextView = findViewById(R.id.estimatedPaintTextView);
+
+        // Retrieve intent
+        Intent mainIntent = getIntent();
+        String gallons = mainIntent.getStringExtra("gallons");
+
+        String newMessage = "Estimated Paint Required: " + gallons + " gallons";
+        estimatedPaintTextView.setText(newMessage);
+    }
+
+    public void inputDimensionsOnClick(View v)
+    {
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent);
     }
 }
--- app/src/main/java/chatch/cs134/miracosta/paintestimator/MainActivity.java
+++ app/src/main/java/chatch/cs134/miracosta/paintestimator/MainActivity.java
@@ -1,10 +1,14 @@
 package chatch.cs134.miracosta.paintestimator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import chatch.cs134.miracosta.paintestimator.Model.InteriorRoom;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -17,12 +21,16 @@ public class MainActivity extends AppCompatActivity
     Button computeButton;
     Button helpButton;
 
+    TextView interiorSurfaceAreaTextView;
+    InteriorRoom room;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Connect and set everything up
         lengthEditText = findViewById(R.id.lengthEditText);
         widthEditText = findViewById(R.id.widthEditText);
         heightEditText = findViewById(R.id.heightEditText);
@@ -31,5 +39,33 @@ public class MainActivity extends AppCompatActivity
 
         computeButton = findViewById(R.id.computeButton);
         helpButton = findViewById(R.id.helpButton);
+        interiorSurfaceAreaTextView = findViewById(R.id.interiorSurfaceAreaTextView);
+
+        interiorSurfaceAreaTextView.setText("");
+
+        room = new InteriorRoom(0,0,0,0,0);
+    }
+
+    public void computeOnClick(View v)
+    {
+        int length = Integer.parseInt(lengthEditText.getText().toString());
+        int width = Integer.parseInt(widthEditText.getText().toString());
+        int height = Integer.parseInt(heightEditText.getText().toString());
+        int doors = Integer.parseInt(doorsEditText.getText().toString());
+        int windows = Integer.parseInt(windowsEditText.getText().toString());
+
+        room = new InteriorRoom(doors, windows, width, height, length);
+
+        // Display interior surface area
+        String textView = "Interior surface area: " + room.totalSurfaceArea();
+        textView += "\nGallons needed: " + room.gallonsOfPaintRequired();
+        interiorSurfaceAreaTextView.setText(textView);
+    }
+
+    public void helpOnClick(View v)
+    {
+        Intent helpIntent = new Intent(this, HelpActivity.class);
+        helpIntent.putExtra("gallons", room.gallonsOfPaintRequired());
+        startActivity(helpIntent);
     }
 }
